cbc1e675ce57c87b93f88508cc622c17f5b40333
JuanToanXoup <Johndoan42@gmail.com>
2020-07-22 20:30:06 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(instances)
+        anagram = []
+        instances.each do |index| 
+            if index.chars.sort == self.word.chars.sort
+                anagram << index
+            end
+        end
+        anagram
+    end
+end
+
